57196eb9d5fa9f8dc509d2d9d621c852cbc8da62
c1ph3r-dev <c1ph3r.dev@gmail.com>
2024-09-22 08:42:05 -0400

    API calls

--- public/scripts/forecast.js
+++ public/scripts/forecast.js
@@ -1,1 +1,24 @@
 const key = 'YOUR_ACCUWEATHER_API_KEY';
+
+// get weather information
+const getWeather = async (cityKey) => {
+
+  const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+  const query = `${cityKey}?apikey=${key}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+};
+
+// get city information
+const getCity = async (city) => {
+  const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+  const query = `?apikey=${key}&q=${city}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+};
