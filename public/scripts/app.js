8edbfa9ff20cd99d5482db8d2079532006418c8c
c1ph3r-dev <c1ph3r.dev@gmail.com>
2024-09-22 09:18:03 -0400

    Updating UI Text

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,4 +1,28 @@
 const cityForm = document.querySelector('form');
+const card = document.querySelector('.card');
+const details = document.querySelector('.details');
+
+const updateUI = (data) => {
+
+  // destructure properties
+  const { cityDetails, weather } = data;
+
+  // update details template
+  details.innerHTML = `
+    <h5 class="my-3">${cityDetails.EnglishName}</h5>
+    <div class="my-3">${weather.WeatherText}</div>
+    <div class="display-4 my-4">
+      <span>${weather.Temperature.Metric.Value}</span>
+      <span>&deg;C</span>
+    </div>
+  `;
+
+  // remove the d-none class if present
+  if(card.classList.contains('d-none')) {
+    card.classList.remove('d-none');
+  }
+
+};
 
 const updateCity = async (city) => {
 
@@ -17,8 +41,6 @@ cityForm.addEventListener('submit', e => {
   cityForm.reset();
 
   // update UI with new city
-  updateCity(city).then(data => {
-
-  })
-  .catch(err => console.log(err));
+  updateCity(city).then(data => updateUI(data))
+    .catch(err => console.log(err));
 });
